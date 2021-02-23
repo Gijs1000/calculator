f6a70cd57da14b0991bbcf7cd897e0d65aef91dd
Gijs Bolhuis <bolhuisgijs@gmail.com>
2021-02-23 13:34:36 +0100

    Added operator function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) { //how well will these functions work with multiple values?
-    return a + b;
+    return a + b; //add a check to see if value is a number
 }
 
 function subtract(a,b) {
@@ -12,4 +12,16 @@ function multiply(a, b) {
 
 function divide(a,b) {
     return a / b;
+}
+
+function operate(operator, numberA, numberB) {
+    if (operator === 'add') {
+        return add(numberA, numberB);
+    } else if (operator === 'subtract') {
+        return subtract(numberA, numberB);
+    } else if (operator === 'multiply') {
+        return multiply(numberA, numberB)
+    } else {
+        return divide(numberA, numberB);
+    }
 }
