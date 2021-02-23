654699b12a6ee4cb779bdf5657cdb0fa6a4921e7
Gijs Bolhuis <bolhuisgijs@gmail.com>
2021-02-23 23:15:19 +0100

    Added function that captures first input from  number buttons

--- script.js
+++ script.js
@@ -31,4 +31,28 @@ function updateDisplay(value) {
     displayedText.textContent = value.toString();
 }
 
-const numberButtons = Array.from(document.getElementsByClassName('numberButton'));
+let input = '';
+const numberButtons = Array.from(document.getElementsByClassName('numberButton')); //adding event listeners to all number buttons
+for (let i = 0; i < numberButtons.length; i++) {
+    let button = numberButtons[i];
+    button.addEventListener('click', () => input += button.textContent); //captures input and stores to input variable
+}
+
+let operator = '';
+const operatorButtons = Array.from(document.getElementsByClassName('operatorButton')); //add event listeners to operator buttons
+for (let i = 0; i < operatorButtons.length; i++) {
+    let button = numberButtons[i];
+    button.addEventListener('click', () => addOperator(button));
+}
+
+function addOperator(button) {
+    if (operator) { //
+        operator = '';
+    }
+    operator += button.id;
+}
+
+
+
+//when user presses an operator (except equals), input so far + the type of operator is stored
+//when user presses equals sign (=), store second part of input and perform the operator on first input & second input (use operate function)
