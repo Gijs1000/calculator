6224f8d576878493dc70ce7dfd7329819f962222
Gijs Bolhuis <bolhuisgijs@gmail.com>
2021-02-23 13:23:47 +0100

    Added arithmetic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) { //how well will these functions work with multiple values?
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
